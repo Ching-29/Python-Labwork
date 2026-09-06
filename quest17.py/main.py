a5a60a82cbdf98d5b4ae28cb190df9451b90aa31
Ching-29 <hchingkheinganbi_ug_25@cse.nits.ac.in>
2026-09-06 14:27:17 +0530

    Initialize main.py with MVC structure

---
+++ quest17.py/main.py
@@ -0,0 +1,15 @@
+from model import EmployeeModel
+from view import EmployeeView
+from controller import EmployeeController
+
+
+def main():
+    model = EmployeeModel()
+    view = EmployeeView()
+    controller = EmployeeController(model, view)
+
+    controller.run()
+
+
+if __name__ == "__main__":
+    main()
